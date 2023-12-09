c98c4fbb0b3d6657103daebfd08627b956f3fc33
vwakesahu <viveksahu1762@gmail.com>
2023-12-09 20:34:24 +0530

    Faltu commit

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,8 +3,9 @@ const path = require('path');
 module.exports = {
   resolve: {
     fallback: {
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "url": require.resolve("url/"),
     },
   },
 };
