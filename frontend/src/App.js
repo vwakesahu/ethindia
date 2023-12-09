ff299a6e22a19d4dd2921ca903a44a25aeb1effb
vwakesahu <viveksahu1762@gmail.com>
2023-12-09 23:26:13 +0530

    Some Changes by Satish

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage, ConnectWallet, Swap } from "./pages/index";
 import { NavBar } from "./components/index";
 import DoTransaction from "./pages/DoTransaction";
+import { Metamask } from "./pages/Metamask";
 const App = () => {
+
+
+
   return (
     <div>
-      {" "}
       <Router>
         <NavBar />
         <div className="mx-16">
@@ -15,6 +18,7 @@ const App = () => {
             <Route path="/swap" element={<Swap />} />
             <Route path="/connect-wallet" element={<ConnectWallet />} />
             <Route path="/transaction" element={<DoTransaction />} />
+            <Route path="/metamask-connect" element={<Metamask />} />
           </Routes>
         </div>{" "}
       </Router>
